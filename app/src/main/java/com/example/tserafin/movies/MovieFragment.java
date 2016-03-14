b36ac8225681dece435e079a796a46f7f2a15863
tserafin <tserafin1991@gmail.com>
2016-03-14 22:17:21 +1100

    Refactored Adapter logic, added Image transfer. Needs testing.

--- app/src/main/java/com/example/tserafin/movies/MovieFragment.java
+++ app/src/main/java/com/example/tserafin/movies/MovieFragment.java
@@ -1,22 +1,43 @@
 package com.example.tserafin.movies;
 
-import android.media.Image;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import com.example.tserafin.movies.adapter.ImageAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieFragment extends Fragment {
 
-    private ArrayAdapter<Image> movieAdapter;
+    private ArrayList<JSONObject> movieDetails;
+    private ImageAdapter movieAdapter;
 
     public MovieFragment() {
     }
@@ -24,7 +45,7 @@ public class MovieFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        //TODO: populate movies
+        retrieveMovies();
     }
 
     @Override
@@ -45,22 +66,164 @@ public class MovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        movieDetails = new ArrayList<>();
+        movieAdapter = new ImageAdapter(
+                getContext()
+        );
 
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridView.setAdapter(movieAdapter);
+        //click listener for going to other activity
+
+        return rootView;
     }
 
     private void retrieveMovies() {
         FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
-        //TODO: Execute FetchMoviesTask
+        fetchMoviesTask.execute(new Pair<>("api_key", getString(R.string.api_key)));
     }
 
-    public class FetchMoviesTask extends AsyncTask<Pair<String,String>, Void, String> {
+    public class FetchMoviesTask extends AsyncTask<Pair<String,String>, Void, Void> {
 
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
         @Override
-        protected String doInBackground(Pair<String, String>... params) {
+        protected Void doInBackground(Pair<String, String>... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonResponse = null;
+            String apiKey = null;
+
+            try {
+                // Set up query
+                URL url;
+                if (params.length > 0) {
+                    String baseUrl = "http://api.themoviedb.org/3/movie/popular";
+                    Uri.Builder builder = Uri.parse(baseUrl).buildUpon();
+                    for (Pair<String, String> param: params) {
+                        builder.appendQueryParameter(param.first, param.second);
+                        if (param.first.equals("api_key")) {
+                            apiKey = param.second;
+                        }
+                    }
+                    url = new URL(builder.build().toString());
+                } else {
+                    throw new MalformedURLException("Missing API Key");
+                }
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                jsonResponse = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                getImagesFromJson(jsonResponse, 20, apiKey);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Error parsing JSON", e);
+            }
             return null;
-            //TODO: Implement API call
+        }
+
+//        @Override
+//        protected void onPostExecute() {
+//            //TODO: traverse through downloaded images directory, set reference array to ids of all files
+//        }
+
+        private void getImagesFromJson(String movieJsonStr, int numMovies, String apiKey) throws JSONException {
+            //JSON Objects
+            final String TMDB_results = "results";
+            final String TMDB_poster = "poster_path";
+
+            //Store JSON results for later use (details screen)
+            movieDetails.clear();
+
+            //Parse JSON
+            JSONObject movieJson = new JSONObject(movieJsonStr);
+            JSONArray moviesArray = movieJson.getJSONArray(TMDB_results);
+            for(int i=0;(i<numMovies)&&(i<moviesArray.length());i++) {
+                JSONObject movie = moviesArray.getJSONObject(i);
+                movieDetails.add(movie);
+                String movieImagePath = movie.getString(TMDB_poster);
+                //Request to get Image
+
+                try {
+                    // Set up query
+                    URL url;
+                    if (apiKey != null) {
+                        String baseUrl = "http://http://image.tmdb.org/t/p/w150";
+                        Uri.Builder builder = Uri.parse(baseUrl).buildUpon();
+                        builder.appendPath(movieImagePath);
+                        url = new URL(builder.build().toString());
+                    } else {
+                        throw new MalformedURLException("Missing API Key");
+                    }
+                    saveImage(url, "test.jpg");
+                } catch (MalformedURLException e) {
+                    Log.e(LOG_TAG, "Incorrectly formed URL", e);
+                    return;
+                }
+            }
+        }
+
+        private void saveImage(URL imageUrl, String destinationFile){
+            InputStream is = null;
+            OutputStream os = null;
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) imageUrl.openConnection();
+                is = urlConnection.getInputStream();
+                os = new FileOutputStream(destinationFile);
+                byte[] b = new byte[2048];
+                int length;
+
+                while ((length = is.read(b)) != -1) {
+                    os.write(b, 0, length);
+                }
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+            } finally {
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                    if (is != null) {
+                        os.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/tserafin/movies/adapter/ImageAdapter.java
@@ -0,0 +1,57 @@
+package com.example.tserafin.movies.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<Integer> mImageIds = new ArrayList<>();
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mImageIds.get(position));
+        return imageView;
+    }
+
+    public void setImageReferences(List<Integer> ids) {
+        mImageIds.clear();
+        mImageIds.addAll(ids);
+    }
+}
